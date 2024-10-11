45fb3ba568405dd5e28c1efecd4bde4fd297efa9
Dlerma98 <davidglerma1990@gmail.com>
2024-10-11 16:53:05 +0200

    Fin paso 15 Traducir utilizando un Json español añadiendo diferentes atributos junto con su correspondiente traduccion

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,5 +1,5 @@
 <x-layout meta-title="Create a new Post" meta-description="Form to create a new Post">
-    <h1>Create a new Post</h1>
+    <h1>{{ __('Create a new Post') }}</h1>
 
 
 
@@ -7,7 +7,7 @@
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
         <label>
-            Title <br />
+            {{__('Title')}} <br />
             <input type="text" name="title"  value="{{old('title')}}">
             @error('title')
             <br />
@@ -16,7 +16,7 @@
         </label>
         <br />
         <label>
-            Body <br />
+            {{__('Body')}} <br />
             <textarea name="body"  {{old('body')}} ></textarea>
 
             @error('body')
@@ -25,8 +25,8 @@
             @enderror
         </label>
         <br />
-        <button type="submit">Send</button>
+        <button type="submit">{{__("Send")}}</button>
         <br/>
     </form>
-    <a href="{{ route('posts.index') }}">Back</a>
+    <a href="{{ route('posts.index') }}">{{__("Back")}}</a>
 </x-layout>
