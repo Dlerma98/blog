a79716f496802cc1d9fa9a7c8465a9d17ebd6c8c
Dlerma98 <davidglerma1990@gmail.com>
2024-10-17 15:49:12 +0200

    Paso 23 medio cambiar estilo en los titulos de paginas

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,10 @@
 <x-layout meta-title="Blog" meta-description="Descripcion de la pagina del blog">
-    <h1>Blog</h1>
+
+    <div class="mx-auto mt-4 max-w-6xl">
+        <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+            Blog
+        </h1>
+    </div>
     <a href="{{route('posts.create')}}">{{__("Create a new Post")}}</a>
     @foreach($posts as $post)
 
