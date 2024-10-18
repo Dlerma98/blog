7e1506aa8aa577763583e074a649c0854ba01769
Dlerma98 <davidglerma1990@gmail.com>
2024-10-18 16:30:48 +0200

    paso 26 utilizando layout

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,9 +1,19 @@
-<x-layout meta-title="Create a new Post" meta-description="Form to create a new Post">
+<x-app-layout meta-title="Create a new Post" meta-description="Form to create a new Post">
 
-    <h1>{{ __('Create a new Post') }}</h1>
-
-
-
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                {{ __('Create a new Post') }}
+            </h2>
+        </x-slot>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
 
     <form method="POST" action="{{ route('posts.store') }}">
         @csrf
@@ -12,4 +22,4 @@
             <button type="submit">{{__("Send")}}</button>
         <br/>
     <a href="{{ route('posts.index') }}">{{__("Back")}}</a>
-</x-layout>
+</x-app-layout>
