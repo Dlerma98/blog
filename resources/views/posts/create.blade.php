30c66d86af44414be5eb2cbebc6c94b273359232
Dlerma98 <davidglerma1990@gmail.com>
2024-10-18 17:07:47 +0200

    paso 27 reduciendo codigo utilizando componentes y layouts

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,17 +9,16 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+
                 </div>
             </div>
         </div>
     </div>
 
     <form method="POST" action="{{ route('posts.store') }}">
-        @csrf
         @include("posts.form-fields")
-        <br />
-            <button type="submit">{{__("Send")}}</button>
-        <br/>
-    <a href="{{ route('posts.index') }}">{{__("Back")}}</a>
+        <x-primary-button type="submit" class="mt-4">{{__('Send')}}</x-primary-button>
+        @csrf
+
+
 </x-app-layout>
